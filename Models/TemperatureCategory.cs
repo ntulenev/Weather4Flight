b5ad7bf2802022e42c830e0c241e45ce3b7a65bb
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-07 18:53:18 +0200

    Add TemperatureCategory enum

---
+++ Models/TemperatureCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace Models;
+
+/// <summary>
+/// Represents a category of temperature.
+/// </summary>
+public enum TemperatureCategory
+{
+    /// <summary>
+    /// The temperature is below normal.
+    /// </summary>
+    Cold,
+
+    /// <summary>
+    /// The temperature is normal.
+    /// </summary>
+    Normal,
+
+    /// <summary>
+    /// The temperature is above normal.
+    /// </summary>
+    Hot
+}
