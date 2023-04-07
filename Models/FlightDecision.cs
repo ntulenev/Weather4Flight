386f5e01240bc7b867e88ea43e2b4fb6ad0506d2
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-07 18:44:52 +0200

    Add FlightDecision flight decision model

---
+++ Models/FlightDecision.cs
@@ -0,0 +1,8 @@
+﻿namespace Models;
+
+/// <summary>
+/// Represents the decision whether a flight is recommended or not, along with the reasons for the decision.
+/// </summary>
+/// <param name="Recomendation">General flight recomendation.</param>
+/// <param name="Reasons">Reasons for recomendation.</param>
+public record FlightDecision(FlightRecomendation Recomendation, NoFlightReasons Reasons);
