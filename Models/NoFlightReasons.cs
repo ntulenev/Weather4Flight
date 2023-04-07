9c3d9960307faf266c20815a96d6cbe7d19fc963
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-07 18:44:12 +0200

    Add NoFlightReasons enum

---
+++ Models/NoFlightReasons.cs
@@ -0,0 +1,30 @@
+﻿namespace Models
+{
+    /// <summary>
+    /// Represents the reasons why a flight may not be recommended.
+    /// </summary>
+    [Flags]
+    public enum NoFlightReasons
+    {
+        /// <summary>
+        /// No reason for not recommending a flight.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The temperature is too cold for flying.
+        /// </summary>
+        ColdTemperature,
+
+        /// <summary>
+        /// The temperature is too hot for flying.
+        /// </summary>
+        HotTemperature,
+
+        /// <summary>
+        /// The wind speed is too high for flying.
+        /// </summary>
+        Wind
+    }
+
+}
