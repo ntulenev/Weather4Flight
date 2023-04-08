1420bfecb60ba14aae88bab567fb67b8520a8075
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-08 21:48:50 +0200

    Add CityName model

---
+++ Models/CityName.cs
@@ -0,0 +1,29 @@
+﻿namespace Models;
+
+/// <summary>
+/// Represents the name of a city.
+/// </summary>
+public class CityName
+{
+    /// <summary>
+    /// Gets the value of the city name.
+    /// </summary>
+    public string Value { get; }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="CityName"/> class.
+    /// </summary>
+    /// <param name="value">The value of the city name.</param>
+    /// <exception cref="ArgumentException">Thrown when the city name is null or empty.</exception>
+    public CityName(string value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException("City name cannot be null or empty");
+        }
+
+        Value = value;
+    }
+}
