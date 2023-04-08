b5be936ac06cb9fe0d0af9cfe218a24fecda2237
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-08 21:30:36 +0200

    Fix namespaces

--- Models/NoFlightReasons.cs
+++ Models/NoFlightReasons.cs
@@ -1,35 +1,33 @@
-﻿namespace Models
+﻿namespace Models;
+
+/// <summary>
+/// Represents the reasons why a flight may not be recommended.
+/// </summary>
+[Flags]
+public enum NoFlightReasons
 {
     /// <summary>
-    /// Represents the reasons why a flight may not be recommended.
+    /// No reason for not recommending a flight.
     /// </summary>
-    [Flags]
-    public enum NoFlightReasons
-    {
-        /// <summary>
-        /// No reason for not recommending a flight.
-        /// </summary>
-        None = 0,
+    None = 0,
 
-        /// <summary>
-        /// The temperature is too cold for flying.
-        /// </summary>
-        ColdTemperature,
+    /// <summary>
+    /// The temperature is too cold for flying.
+    /// </summary>
+    ColdTemperature,
 
-        /// <summary>
-        /// The temperature is too hot for flying.
-        /// </summary>
-        HotTemperature,
+    /// <summary>
+    /// The temperature is too hot for flying.
+    /// </summary>
+    HotTemperature,
 
-        /// <summary>
-        /// The wind speed is too high for flying.
-        /// </summary>
-        Wind,
+    /// <summary>
+    /// The wind speed is too high for flying.
+    /// </summary>
+    Wind,
 
-        /// <summary>
-        /// Precipitations that could affect flight.
-        /// </summary>
-        ImportantPrecipitations
-    }
-
+    /// <summary>
+    /// Precipitations that could affect flight.
+    /// </summary>
+    ImportantPrecipitations
 }
