90b65c432283131d39209dc7a2324d02e4873a0e
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 14:43:00 +0200

    Add XMLDoc for IOpenWeatherLoader

--- OpenWeatherMap/Logic/IOpenWeatherLoader.cs
+++ OpenWeatherMap/Logic/IOpenWeatherLoader.cs
@@ -2,7 +2,16 @@
 
 namespace OpenWeatherMap.Logic;
 
+/// <summary>
+/// Represents a service for loading weather forecast data from OpenWeatherMap API.
+/// </summary>
 public interface IOpenWeatherLoader
 {
+    /// <summary>
+    /// Asynchronously retrieves a weather forecast for the specified city.
+    /// </summary>
+    /// <param name="city">The name of the city to retrieve the forecast for.</param>
+    /// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation that returns a <see cref="WeatherForecast"/> object.</returns>
     Task<WeatherForecast> GetWeatherForecastAsync(string city, CancellationToken cancellationToken = default);
 }
