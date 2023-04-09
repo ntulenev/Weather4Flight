873b8e86a812718446c16304d8d4c55328baffd7
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 10:52:01 +0200

    Add DTO WeatherData

---
+++ OpenWeatherMap/DTO/WeatherData.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenWeatherMap.DTO;
+
+public class WeatherData
+{
+    [JsonPropertyName("dt_txt")]
+    public string DateText { get; set; } = default!;
+
+    [JsonPropertyName("wind")]
+    public WindData Wind { get; set; } = default!;
+
+    [JsonPropertyName("main")]
+    public MainData Main { get; set; } = default!;
+
+    [JsonPropertyName("weather")]
+    public List<Weather> Weather { get; set; } = default!;
+}
