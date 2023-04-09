113d2d9e05995ed13aefa63173c361f9959e96df
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 10:52:10 +0200

    Add DTO WeatherForecast

---
+++ OpenWeatherMap/DTO/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenWeatherMap.DTO
+{
+    public class WeatherForecast
+    {
+        [JsonPropertyName("city")]
+        public City City { get; set; } = default!;
+
+        [JsonPropertyName("list")]
+        public List<WeatherData> WeatherDataList { get; set; } = default!;
+    }
+}
