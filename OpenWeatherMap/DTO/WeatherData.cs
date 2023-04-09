514774c03f3b10c0084785bb29b3943a6cc89413
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 11:55:58 +0200

    Add WeatherData XMLDoc

--- OpenWeatherMap/DTO/WeatherData.cs
+++ OpenWeatherMap/DTO/WeatherData.cs
@@ -2,17 +2,33 @@
 
 namespace OpenWeatherMap.DTO;
 
+/// <summary>
+/// Represents weather data returned by the OpenWeatherMap API for a specific date and time.
+/// </summary>
 public class WeatherData
 {
+    /// <summary>
+    /// The date and time of the weather data in UTC.
+    /// </summary>
     [JsonPropertyName("dt_txt")]
     public string DateText { get; set; } = default!;
 
+    /// <summary>
+    /// The wind data.
+    /// </summary>
     [JsonPropertyName("wind")]
     public WindData Wind { get; set; } = default!;
 
+    /// <summary>
+    /// The main temperature data.
+    /// </summary>
     [JsonPropertyName("main")]
     public MainData Main { get; set; } = default!;
 
+    /// <summary>
+    /// The list of weather conditions.
+    /// </summary>
     [JsonPropertyName("weather")]
     public List<Weather> Weather { get; set; } = default!;
 }
+
