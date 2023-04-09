73531ded042706e4e60d2366d88d8803a092f673
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 11:55:32 +0200

    Add MainData XMLDoc

--- OpenWeatherMap/DTO/MainData.cs
+++ OpenWeatherMap/DTO/MainData.cs
@@ -2,8 +2,15 @@
 
 namespace OpenWeatherMap.DTO;
 
+/// <summary>
+/// Represents the main temperature data returned by the OpenWeatherMap API.
+/// </summary>
 public class MainData
 {
+    /// <summary>
+    /// The temperature in Celsius.
+    /// </summary>
     [JsonPropertyName("temp")]
     public double Temp { get; set; }
 }
+
