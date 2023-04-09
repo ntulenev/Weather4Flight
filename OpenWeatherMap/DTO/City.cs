71930361772c73faa5ce1108de97d9935893c3fd
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 11:55:24 +0200

    Add City XMLDoc

--- OpenWeatherMap/DTO/City.cs
+++ OpenWeatherMap/DTO/City.cs
@@ -2,8 +2,15 @@
 
 namespace OpenWeatherMap.DTO;
 
+/// <summary>
+/// Represents a city returned by the OpenWeatherMap API.
+/// </summary>
 public class City
 {
+    /// <summary>
+    /// The name of the city.
+    /// </summary>
     [JsonPropertyName("name")]
     public string Name { get; set; } = default!;
 }
+
