6a93fb036ebc8a92d2981fb5d1487e47e24458e7
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 19:45:52 +0200

    Add OpenWeatherLoader validation and XMLDoc

--- OpenWeatherMap/Logic/OpenWeatherLoader.cs
+++ OpenWeatherMap/Logic/OpenWeatherLoader.cs
@@ -1,29 +1,54 @@
-﻿using OpenWeatherMap.DTO;
+﻿using Microsoft.Extensions.Options;
+
+using OpenWeatherMap.Configuration;
+using OpenWeatherMap.DTO;
 
 using System.Text.Json;
 
 namespace OpenWeatherMap.Logic;
 
+/// <summary>
+/// Loads weather forecast from the OpenWeatherMap API using the provided API key and city name.
+/// </summary>
 public class OpenWeatherLoader : IOpenWeatherLoader
 {
-    private readonly HttpClient _httpClient;
-    private readonly string _apiKey;
-
-    public OpenWeatherLoader(HttpClient httpClient, string apiKey)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OpenWeatherLoader"/> class.
+    /// </summary>
+    /// <param name="httpClient">The HTTP client used to make API requests.</param>
+    /// <param name="options">The options containing the OpenWeatherMap API key and URL.</param>
+    /// <exception cref="System.ArgumentNullException">Thrown when the httpClient or options parameters are null.</exception>
+    /// <exception cref="System.ArgumentException">Thrown when the options value is not set.</exception>
+    public OpenWeatherLoader(HttpClient httpClient, IOptions<OpenWeatherMapConfiguration> options)
     {
-        // TODO Add validation
-        // TODO Add Options<>
+
+        ArgumentNullException.ThrowIfNull(httpClient);
+        ArgumentNullException.ThrowIfNull(options);
+
+        if (options.Value is null)
+        {
+            throw new ArgumentException("Options value is not set", nameof(options));
+        }
+
         _httpClient = httpClient;
-        _apiKey = apiKey;
+        _apiKey = options.Value.ApiKey;
+        _apiPath = options.Value.ApiUrl;
     }
 
+    /// <inheritdoc/>
+    /// <exception cref="System.ArgumentNullException">Thrown when the city parameter is null.</exception>
+    /// <exception cref="System.ArgumentException">Thrown when the city parameter is empty or whitespace.</exception>
     public async Task<WeatherForecast> GetWeatherForecastAsync(string city, CancellationToken cancellationToken = default)
     {
-        // TODO Add validation
-        // TODO Move url to Options config
-        string url = $"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={_apiKey}&units=metric";
+        ArgumentNullException.ThrowIfNull(city);
+        ArgumentException.ThrowIfNullOrEmpty(city);
+
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var url = string.Format(_apiPath, city, _apiKey);
+
         HttpResponseMessage response = await _httpClient.GetAsync(url, cancellationToken);
-        string responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
+        var responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
 
         if (response.IsSuccessStatusCode)
         {
@@ -31,8 +56,12 @@ public class OpenWeatherLoader : IOpenWeatherLoader
         }
         else
         {
-            // TODO Add custom exception type 
+            // TODO Add custom exception type
             throw new Exception($"Error retrieving weather forecast: {responseBody}");
         }
     }
+
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+    private readonly string _apiPath;
 }
