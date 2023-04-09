ebcc453554fdbcaff804200eca35a533c3eff069
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 11:55:40 +0200

    Add WeatherForecast XMLDoc

--- OpenWeatherMap/DTO/WeatherForecast.cs
+++ OpenWeatherMap/DTO/WeatherForecast.cs
@@ -1,13 +1,22 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace OpenWeatherMap.DTO
-{
-    public class WeatherForecast
-    {
-        [JsonPropertyName("city")]
-        public City City { get; set; } = default!;
+namespace OpenWeatherMap.DTO;
 
-        [JsonPropertyName("list")]
-        public List<WeatherData> WeatherDataList { get; set; } = default!;
-    }
+/// <summary>
+/// Represents a weather forecast returned by the OpenWeatherMap API.
+/// </summary>
+public class WeatherForecast
+{
+    /// <summary>
+    /// The city for which the weather forecast is for.
+    /// </summary>
+    [JsonPropertyName("city")]
+    public City City { get; set; } = default!;
+
+    /// <summary>
+    /// The list of weather data for the forecast.
+    /// </summary>
+    [JsonPropertyName("list")]
+    public List<WeatherData> WeatherDataList { get; set; } = default!;
 }
+
