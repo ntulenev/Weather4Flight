195f5fa94a20c5dc3fdaa937feea4f1d435977a8
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-09 11:56:05 +0200

    Add Weather XMLDoc

--- OpenWeatherMap/DTO/Weather.cs
+++ OpenWeatherMap/DTO/Weather.cs
@@ -2,8 +2,14 @@
 
 namespace OpenWeatherMap.DTO;
 
+/// <summary>
+/// Represents the weather data returned by the OpenWeatherMap API.
+/// </summary>
 public class Weather
 {
+    /// <summary>
+    /// The main weather condition.
+    /// </summary>
     [JsonPropertyName("main")]
     public string Main { get; set; } = default!;
 }
