8ef34ff332747454ca70337501afd982fd126fca
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-10 18:05:21 +0200

    Add SystemTextJsonSerializer tests

---
+++ OpenWeatherMap.Tests/SystemTextJsonSerializer.Tests.cs
@@ -0,0 +1,67 @@
+﻿using OpenWeatherMap.Serialization;
+
+using FluentAssertions;
+
+using OpenWeatherMap.DTO;
+
+namespace OpenWeatherMap.Tests;
+
+public class SystemTextJsonSerializerTests
+{
+    [Fact(DisplayName = "Deserialize should throw ArgumentNullException when json is null")]
+    public void Deserialize_Should_Throw_ArgumentNullException_When_Json_Is_Null()
+    {
+        // Arrange
+        var serializer = new SystemTextJsonSerializer();
+        string json = null!;
+
+        // Act
+        var action = new Action(() => serializer.Deserialize<string>(json));
+
+        // Assert
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = "Deserialize should throw ArgumentException when json is empty")]
+    public void Deserialize_Should_Throw_ArgumentException_When_Json_Is_Empty()
+    {
+        // Arrange
+        var serializer = new SystemTextJsonSerializer();
+        var json = "";
+
+        // Act
+        var action = new Action(() => serializer.Deserialize<string>(json));
+
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact(DisplayName = "Deserialize should throw ArgumentException when json is whitespace")]
+    public void Deserialize_Should_Throw_ArgumentException_When_Json_Is_Whitespace()
+    {
+        // Arrange
+        var serializer = new SystemTextJsonSerializer();
+        var json = "   ";
+
+        // Act
+        var action = new Action(() => serializer.Deserialize<string>(json));
+
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact(DisplayName = "Deserialize should deserialize json string to object of type T")]
+    public void Deserialize_Should_Deserialize_Json_String_To_Object_Of_Type_T()
+    {
+        // Arrange
+        var serializer = new SystemTextJsonSerializer();
+        var json = "{\"name\":\"New York\"}";
+
+        // Act
+        var city = serializer.Deserialize<City>(json);
+
+        // Assert
+        city.Should().NotBeNull();
+        city.Name.Should().Be("New York");
+    }
+}
