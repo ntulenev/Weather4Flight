efd3194711cc6b7046f701e3383d826456514171
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-10 18:25:59 +0200

    Add MainData tests

---
+++ OpenWeatherMap.Tests/MainData.Tests.cs
@@ -0,0 +1,22 @@
+﻿using OpenWeatherMap.DTO;
+using System.Text.Json;
+using FluentAssertions;
+
+namespace OpenWeatherMap.Tests;
+
+public class MainDataTests
+{
+    [Fact(DisplayName = "Deserialize should return a MainData object when given valid JSON")]
+    public void Deserialize_Should_Return_MainData_Object_When_Given_Valid_JSON()
+    {
+        // Arrange
+        var json = "{\"temp\": 20.5}";
+
+        // Act
+        var mainData = JsonSerializer.Deserialize<MainData>(json);
+
+        // Assert
+        mainData.Should().NotBeNull();
+        mainData!.Temp.Should().Be(20.5);
+    }
+}
