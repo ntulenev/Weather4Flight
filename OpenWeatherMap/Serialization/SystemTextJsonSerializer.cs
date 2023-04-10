9fc778558175a893c22578200cec8708c70ea218
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-10 06:51:42 +0200

    Add SystemTextJsonSerializer

---
+++ OpenWeatherMap/Serialization/SystemTextJsonSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace OpenWeatherMap.Serialization;
+
+public class SystemTextJsonSerializer : IJsonSerializer
+{
+    public T Deserialize<T>(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<T>(json)!;
+    }
+}
