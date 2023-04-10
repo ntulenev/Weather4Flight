5494d73a5c1464a29a471d3d96c3a86e9376f5c9
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-10 18:26:07 +0200

    Add WindData tests

---
+++ OpenWeatherMap.Tests/WindData.Tests.cs
@@ -0,0 +1,22 @@
+﻿using OpenWeatherMap.DTO;
+using System.Text.Json;
+using FluentAssertions;
+
+namespace OpenWeatherMap.Tests;
+
+public class WindDataTests
+{
+    [Fact(DisplayName = "Deserialize should return a WindData object when given valid JSON")]
+    public void Deserialize_Should_Return_WindData_Object_When_Given_Valid_JSON()
+    {
+        // Arrange
+        var json = "{\"speed\": 3.2}";
+
+        // Act
+        var windData = JsonSerializer.Deserialize<WindData>(json);
+
+        // Assert
+        windData.Should().NotBeNull();
+        windData!.Speed.Should().Be(3.2);
+    }
+}
