880cb7682975c53a1e200ae7d680879ca403b3a8
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-10 06:54:00 +0200

    Add XMLDoc for serializers

--- OpenWeatherMap/Serialization/IJsonSerializer.cs
+++ OpenWeatherMap/Serialization/IJsonSerializer.cs
@@ -1,6 +1,15 @@
 ﻿namespace OpenWeatherMap.Serialization;
 
+/// <summary>
+/// Interface for a JSON serializer.
+/// </summary>
 public interface IJsonSerializer
 {
+    /// <summary>
+    /// Deserializes JSON data into an object of type T.
+    /// </summary>
+    /// <typeparam name="T">The type of object to deserialize the JSON into.</typeparam>
+    /// <param name="json">The JSON data to deserialize.</param>
+    /// <returns>The deserialized object.</returns>
     T Deserialize<T>(string json);
 }
--- OpenWeatherMap/Serialization/SystemTextJsonSerializer.cs
+++ OpenWeatherMap/Serialization/SystemTextJsonSerializer.cs
@@ -2,13 +2,18 @@
 
 namespace OpenWeatherMap.Serialization;
 
+/// <summary>
+/// Implementation of the <see cref="IJsonSerializer"/> interface using System.Text.Json.
+/// </summary>
 public class SystemTextJsonSerializer : IJsonSerializer
 {
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentNullException">Thrown when the json parameter is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when the json parameter is empty or whitespace.</exception>
     public T Deserialize<T>(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
         ArgumentException.ThrowIfNullOrEmpty(json);
-
         return JsonSerializer.Deserialize<T>(json)!;
     }
 }
