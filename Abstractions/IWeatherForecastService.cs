a69fca32865c4f7957c2ef227b749443391fb586
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-11 18:28:44 +0200

    Add IWeatherForecastService

---
+++ Abstractions/IWeatherForecastService.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+
+namespace Abstractions;
+
+/// <summary>
+/// Represents a service that can load weather forecasts for cities.
+/// </summary>
+public interface IWeatherForecastService
+{
+    /// <summary>
+    /// Asynchronously loads the weather forecast for the specified city.
+    /// </summary>
+    /// <param name="cityName">The name of the city to load the forecast for.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The result of the task contains the weather forecast for the specified city.</returns>
+    Task<WeatherForecast> LoadWeatherForecastAsync(CityName cityName, CancellationToken cancellationToken);
+}
