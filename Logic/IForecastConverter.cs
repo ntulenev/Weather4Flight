2308e1f54e31b6ac4a557b492b299344efaf7a6d
Nikita Tulenev <n1tulenev@gmail.com>
2023-04-12 20:29:38 +0200

    Add IForecastConverter

---
+++ Logic/IForecastConverter.cs
@@ -0,0 +1,16 @@
+﻿using DTO = OpenWeatherMap.DTO;
+
+namespace Logic;
+
+/// <summary>
+/// Converts a DTO weather forecast to a model weather forecast.
+/// </summary>
+public interface IForecastConverter
+{
+    /// <summary>
+    /// Converts a DTO weather forecast to a model weather forecast.
+    /// </summary>
+    /// <param name="weatherForecast">The DTO weather forecast to convert.</param>
+    /// <returns>The converted model weather forecast.</returns>
+    Models.WeatherForecast Convert(DTO.WeatherForecast weatherForecast);
+}
